20a379b20cd3b3c5cd5a59b486dc7b81037e30ab
Alex <korneev.ry@yandex.ru>
2019-12-08 23:49:57 +0300

    наделал бредятины

--- Task3/client.c
+++ Task3/client.c
@@ -55,8 +55,10 @@ int main(int argc, char **argv) {
     }
 
     bzero(preDir, sizeof(preDir));
-    strcpy(preDir, "~$");
+    strcpy(preDir, "$>");
 
+    struct sockaddr_in tempServerInfo;
+    struct Message msg;
     char inputBuf[SIZE_MSG];
     for (;;) {
         fprintf(stdout, "%s", preDir);
@@ -68,18 +70,108 @@ int main(int argc, char **argv) {
             break;
         }
 
-        if (safeSendMsg(sock, serverInfo, CODE_CONNECT, "CONNECT", strlen("CONNECT")) == -1){
-            logError("Проблемы с отправкой команды на сервер. Соединение разорвано!\n");
+        if (safeSendMsg(sock, serverInfo, CODE_CONNECT, "CONNECT", strlen("CONNECT")) < 0){
+            logError("Проблемы с отправкой команды на сервер - 1. Соединение разорвано!\n");
             break;
         }
 
-        if (execCommand(sock) == 0) {
+        if (safeReadMsg(sock, &tempServerInfo, &msg) < 0) {
+            logError("Проблемы с отправкой команды на сервер - 2. Соединение разорвано!\n");
+            break;
+        }
+
+        
+//---дичь
+        char *sep = " ";
+        char *arg = strtok(inputBuf, sep);
+    
+        if (!strcmp(arg, "load")) {
+
+        } else if (!strcmp(arg, "dload")) {
+
+        }
+
+        if(arg == NULL){
+            sprintf(errorString, "Команда: %s - не поддается парсингу.\nВведите корректную команду. Используйте: help\n", msg->data);
+            return -1;
+        }
+//-------------
+        
+        if (safeSendMsg(sock, &tempServerInfo, CODE_CMD, inputBuf, strlen(inputBuf)) < 0) {
+            logError("Проблемы с отправкой команды на сервер - 3. Соединение разорвано!\n");
+            break;
+        }
+
+        if (execCommand(sock, &tempServerInfo) == 0) {
+            logError("Проблемы с сетью! Соединение разорвано.\n");
             break;
         }
     }
 
     close(sock;)
-    logInfo("Приложение завершило свою работу. До скорой встречи!");
+    logInfo("Приложение завершило свою работу. До скорой встречи!\n");
 
     return 0;
+}
+
+void execCommand(const int sock, struct sockaddr_in *serverInfo) {
+    struct Message msg;
+    int code = -1;
+
+    if(safeReadMsg(sock, serverInfo, &msg) < 0) {
+        logError("Проблемы с получением ответа от сервера. Соединение разорвано!\n");
+        return 0;
+    }
+
+    code = package.code;
+
+    if (code == CODE_ERROR) {
+        logError("Возникла ошибка!\n%s\n", msg.data);
+    } else if (code == CODE_INFO) {
+        logInfo("%s\n", msg.data);
+    } else if (code == CODE_FILE_REQUEST) {
+        loadFile(sock, &tempServerInfo, msg.data);
+    } else if (code == CODE_FILE_AVAILABLE) {
+        downloadFile(sock, &tempServerInfo, msg.data);
+    } else if (code == CODE_OK) {
+        return;
+    } else {
+        logError("Пришло что-то не то! Попробуйте еще раз.");
+    }
+
+    return;
+}
+
+/**
+Отправка файла серверу.
+Входные значения:
+    int sock - соединение с сервером;
+    char *fileName - путь к файлу на стороне клиента.
+*/
+void loadFile(const int sock, struct sockaddr_in tempServerInfo, char *fileName){
+    if(isWho(fileName) != 1){
+        fprintf(stderr, "%s - это не файл!", fileName);
+        sendPack(sock, CODE_ERROR, strlen("Отмена.") + 1, "Отмена.");
+        return;
+    }
+    FILE *file = fopen(fileName, "rb");
+    if(file == NULL){
+        fprintf(stderr, "Не удалось загрузить файл - %s\n", fileName);
+        sendPack(sock, CODE_ERROR, strlen("Отмена.") + 1, "Отмена.");
+        return;
+    }
+    char section[SIZE_MSG] = {'\0'};
+    int res = 0, err;
+    while((res = fread(section, sizeof(char), sizeof(section), file)) != 0){
+        fprintf(stdout, "res = %d\n", res);
+        err = sendPack(sock, CODE_FILE_SECTION, res, section); //TODO обработать ошибку
+        if(err == -1){
+            fprintf(stderr, "Не удалось отправить файл - %s\n", fileName);
+            fclose(file);
+            return;
+        }
+        bzero(section, sizeof(section));
+    }
+    fclose(file);
+    sendPack(sock, CODE_FILE_END, strlen("Файл отправлен полностью.") + 1, "Файл отправлен полностью.");
 }
